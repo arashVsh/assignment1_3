24a718d42abfdaedc2b869e1b8c44e028c62fccd
arashVsh <arash.vashagh@gmail.com>
2024-12-06 19:34:15 -0400

    More Clarity

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 classes = ["unacc", "acc", "good", "vgood"]
-n_classes = 4
 
 
 class MyNaiveBayes:
@@ -15,10 +14,10 @@ class MyNaiveBayes:
         # Calculate class probabilities
         for cls in classes:
             cls_samples = []
-            for i in range(total_samples):
-                if y.values[i][0] == cls:
-                    cls_samples.append(X.values[i])
-                    
+            for row in range(total_samples):
+                if y.values[row][0] == cls:
+                    cls_samples.append(X.values[row])
+
             self.class_probs[cls] = (
                 len(cls_samples) / total_samples
             )  # Probability of class 'cls'
@@ -27,53 +26,52 @@ class MyNaiveBayes:
             self.feature_probs[cls] = {}
 
             # Calculate feature probabilities
-            col_number = 0
-            for col in X.columns:
-                unique_values = X[col].unique()  # Unique values for feature 'col'
+            feature_number = 0
+            for feature in X.columns:
+                unique_values = X[feature].unique()  # Unique values for this feature
                 value_counts = {value: 0 for value in unique_values}
-                m = len(unique_values)  # Number of unique values in column 'col'
 
                 for cls_sample in cls_samples:
-                    value_counts[cls_sample[col_number]] += 1
-                    
+                    value_counts[cls_sample[feature_number]] += 1
+
                 # Store probabilities for each unique value in this feature
-                self.feature_probs[cls][col] = {}
+                self.feature_probs[cls][feature] = {}
                 for value in unique_values:
-                    count = value_counts.get(value, 0)  # Observed count
+                    count = value_counts.get(value, 0)
                     smoothed_prob = (count + 1) / (
-                        len(cls_samples) + m
+                        len(cls_samples) + len(unique_values)
                     )  # As mentioned in class notes
-                    self.feature_probs[cls][col][value] = smoothed_prob
-                
-                col_number += 1
+                    self.feature_probs[cls][feature][value] = smoothed_prob
+
+                feature_number += 1
 
     def predict(self, X):
         predictions = []
         X_array = X.values
-        n_rows, n_cols = X_array.shape
+        n_rows, n_features = X_array.shape
 
-        for i in range(n_rows):
-            row = X_array[i]
+        for row in range(n_rows):
+            row = X_array[row]
             class_scores = {}
 
             # Compute scores for each class
             for cls in classes:
                 class_score = np.log(self.class_probs[cls])
-                for j in range(n_cols):
-                    col = X.columns[j]
-                    feature_value = row[j]
+                for feature_index in range(n_features):
+                    feature = X.columns[feature_index]
+                    feature_value = row[feature_index]
                     class_score += np.log(
-                        self.feature_probs[cls][col].get(feature_value, 1e-9)
+                        self.feature_probs[cls][feature].get(feature_value, 1e-9)
                     )
                 class_scores[cls] = class_score
 
             # Select class with the highest score
             best_class = None
-            max_score = float("-inf")
+            best_score = float("-inf")
             for cls, score in class_scores.items():
-                if score > max_score:
+                if score > best_score:
                     best_class = cls
-                    max_score = score
+                    best_score = score
 
             predictions.append(best_class)
 
